4eac807c86e1db34c3eaf28e1ad4a189f98ef16b
danichagas <chagasdaniel788@gmail.com>
2024-11-12 16:31:12 -0300

    feat: created get and setters

--- src/entities/Bank.java
+++ src/entities/Bank.java
@@ -1,4 +1,33 @@
 package entities;
 
 public class Bank {
+    private int number;
+    private String holder;
+    private double deposit;
+
+    public Bank() {
+    }
+
+    public Bank(int number, String holder, double deposit) {
+        this.number = number;
+        this.holder = holder;
+        this.deposit = deposit;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public double getDeposit() {
+        return deposit;
+    }
+
 }
